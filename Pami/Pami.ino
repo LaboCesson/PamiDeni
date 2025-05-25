cae36087dc5bccba33a12aaa750e99768f846253
TechnoSpiritMC <127853301+TechnoSpiritMC@users.noreply.github.com>
2025-05-25 09:02:31 +0200

    Add files via upload

--- Pami/Pami.ino
+++ Pami/Pami.ino
@@ -1,8 +1,47 @@
 #include "arduino.h"
 #include "LibPami.h"
 #include "MemoryFree.h"
+#include "Lib433Mhz.h"
+
+
+#define TeamSelector PAMI_GPIO_1
+#define Head PAMI_GPIO_2
+#define Guitare PAMI_GPIO_3
+#define Gpio4 PAMI_GPIO_4
+constexpr short Tcooldown = 1000;
+
+#define defaultAngle 90
 
 LibPami pami;
+
+//short angleChange[] = {90, 0, 0, -90};
+
+bool zbeub = false;
+bool coOverride = false;
+bool awaitingDistance = false;
+bool cc = true;
+bool dance = false;
+bool awaitingStart = true;
+
+unsigned long startTime;
+unsigned long beforeStart = 8500; // Example delay before start (85s) METTRE A 85,000 AVANT LE DEBUT DE LA COMPETITIONNNNN
+unsigned long timeout = beforeStart + 15000;    // Example timeout (100s)
+unsigned long timedAction = ~0;    // Proper initialization
+
+char timedActionSelector = 1;
+
+char targetAngleOffset = 0;
+
+constexpr char slopeMin = 0x08;
+constexpr char slopeMax = 0x0f;  
+
+char SINE = 0;
+
+typedef enum {
+  RADIO_NO_ORDER = 0, ///< Aucun message de reçu
+  RADIO_START    = 1, ///< Message de départ
+  RADIO_STOP     = 2, ///< Message d'arrêt
+} t_radioOrder;
 
 
 // Selection d'axe de Gyroscope.
@@ -15,51 +54,24 @@ LibPami pami;
 //
 // 0x03      Mesure effectuée
 //           Sur l'axe Z.
-char axis = 0x02;
+char axis = 0x01;
+char actionSelector = 0x05;
+char direction = -0x01;
 
 
-// Sélection de direction.
-// VALEUR    RÉSULTAT
-// 0x01      Le robot va avancer
-//           en avant.
-//
-// -0x01     Le robot va avancer
-//           en arrière.
-char direction = 0x01;
 
 
-// Selection de vitesse initiale.
-// V ∈ [-0x64 : 0x64]
-// |!| 
-// V < 0 possible mais pas recommandé.
-// Si mauvaise direction, changer le
-// paramètre "direction" [LIGNE 29]
-// Recommandé:
-// V ∈ [-0x20 : 0x40]
-char baseSpeed = 0x20;
+char baseSpeed = 0x55;
 
 
-// Selection du cooldown entre
-// deux corrections, pour faciliter
-// la lecture des messages de debug.
-//
-// Si pour de vrai:
-// T ∈ [0x00 : 0x0a]
-//
-// Si pour un test:
-// V ∈ [0x1e : 0x32]
-short cooldown = 0x32;
 
 
-// Facteur de division
-// Sert à réduire la sensibilité
-// Du robot pour les angles.
-// Valeurs conseillées:
-// X ∈ [-0x02 : 0x0a]
-char divisionFactor = 0x05;
-
+short cooldown = 0x00;
+char divisionFactor = 1;
+char maxSpeed = 0xff;
 
 float offset = 0;  // Corrected declaration
+bool cparti = false;
 
 
 void setup() {
@@ -72,20 +84,21 @@ void setup() {
   pami.afficheur.displayString("init");
   pami.ultrason.begin();
   pami.ultrason.setMaxDistance(500);
-  pami.gpio.configure(PAMI_GPIO_1, PAMI_GPIO_OUTPUT, 1);
-  pami.gpio.configure(PAMI_GPIO_2, PAMI_GPIO_INPUT);
-  pami.gpio.configure(PAMI_GPIO_3, PAMI_GPIO_PWM, 0);
-  pami.chrono.begin();
   pami.gyro.begin();
   pami.gyro.selectAxis(GYROSCOPE_AXIS_X);
   pami.radio.begin(16);
   pami.moteur.setPwmMode(false);
 
   pami.afficheur.displayString("done");
-  delay(2000);
+  delay(200);
+  pami.gyro.display(true);
 
   pami.chrono.display(true);
 
+  pami.gpio.configure(Guitare, PAMI_GPIO_PWM, defaultAngle);
+  pami.gpio.configure(Head, PAMI_GPIO_PWM, defaultAngle);
+  pami.gpio.configure(TeamSelector, PAMI_GPIO_INPUT, defaultAngle);
+  pami.gpio.configure(Gpio4, PAMI_GPIO_UNUSED, defaultAngle);
 
   // Définition de l'angle de départ,
   // Et calculs pour en déduire l'offset
@@ -107,29 +120,111 @@ void setup() {
     break;
     
   default:
-    Serial.println("Failed to identify the selected axis. Please refer to [LINE 18] to select the desired axis.");
+    Serial.println("Incorrect axis.");
     initialAngle = -1;
     return;
   }
 
-    offset = 0 - initialAngle; // Calcule ne nombre à soustraire des angles mesurés pour avoir un écart de l'angle initial.
+  offset = 0 - initialAngle; // Calcule ne nombre à soustraire des angles mesurés pour avoir un écart de l'angle initial.
+
+  pami.afficheur.displayString("jatt");
+
+  while (!cparti) {
+    cparti = (gestionRadio() == RADIO_START);
+  }
+
+  startTime = millis();
+  timeout = timeout + startTime;
+
 }
 
 void loop() {
-  pami.gestion();
+  pami.gestion();  
+  
+  if (!awaitingStart) {
+  
+    //Serial.print("# Gyroscope #");
+    //Serial.print("  Axe X=");
+    //Serial.print(pami.gyro.getAngle(GYROSCOPE_AXIS_X));
+    //Serial.print(", Axe Y=");
+    //Serial.print(pami.gyro.getAngle(GYROSCOPE_AXIS_Y));
+    //Serial.print(", Axe Z=");
+    //Serial.println(pami.gyro.getAngle(GYROSCOPE_AXIS_Z));
+  
+    if (!dance) {
+      if (cc) {conditionnalCorection();}
+      correctDependingOnAngle();
+      onEdge();
+      timedActionsManager();
+    }
+  
+    //Serial.print("Selected Side: ");
+    //Serial.println(pami.gpio.get(TeamSelector));
+  
+    if (pami.gpio.get(TeamSelector) == 1) {
+      actionSelector = 0x05;
+    } else {
+      actionSelector = 0x04;
+    }
+  
+    if (dance){
+      nonMandatoryActions();
+      SINE = (char)(sin(millis() / 1000.0) * 45);
+    }
+  
+    //delay(cooldown);
+  
+    //short distance = 0; // OBTENSION DE LA DISTANCE AVEC LE ULTRASON ICI!
+    //distance = pami.ultrason.getDistance();
+    //Serial.print("Distance: ");
+    //Serial.println(distance);
+  
+    
+  } else {
+    pami.afficheur.displayValue((int)((beforeStart - (millis() - startTime)) / 1000)); // Remaining time in seconds
+    if ((millis() - startTime) >= beforeStart) {
+        awaitingStart = false;
+        Serial.println("Started!");
+    }
+  }
 
-  Serial.print("# Gyroscope #");
-  Serial.print("  Axe X=");
-  Serial.print(pami.gyro.getAngle(GYROSCOPE_AXIS_X));
-  Serial.print(", Axe Y=");
-  Serial.print(pami.gyro.getAngle(GYROSCOPE_AXIS_Y));
-  Serial.print(", Axe Z=");
-  Serial.println(pami.gyro.getAngle(GYROSCOPE_AXIS_Z));
-
-  correctDependingOnAngle();
-
-  delay(cooldown);
+  if ((millis() - startTime) >= timeout) {
+    Serial.println("Timeout.");
+    dance = true;
+    timeout = ~0;
+  }
 }
+
+
+
+
+
+
+void timedActionsManager(){
+  if (timedAction == ~0) {
+    return;
+  }
+
+  if (millis() > timedAction) {
+      if (timedActionSelector == 1) {
+        divisionFactor = 1;
+        baseSpeed = 30;
+        timedAction = -1;
+        cc = false;
+        awaitingDistance = true;
+
+        Serial.println("texec");
+      }
+  }
+}
+
+
+
+
+
+
+
+
 
 void correctDependingOnAngle() {
   float angle;
@@ -148,17 +243,183 @@ void correctDependingOnAngle() {
       break;
 
     default:
-      Serial.println("Failed to identify the selected axis. Please refer to [LINE 19] to select the desired axis.");
+      //Serial.println("Invalid Axis");
       angle = -1;
       return;
   }
 
-  Serial.print("Moteur Gauche : ");
-  Serial.print(direction * (baseSpeed - angle / divisionFactor));
+  if (!coOverride){
+    short md = max(min(direction * (baseSpeed - (angle + targetAngleOffset)), 100), -100);
+    short mg = max(min(direction * (baseSpeed + (angle + targetAngleOffset)), 100), -100);
 
-  Serial.print("       Moteur Droit : ");
-  Serial.println(direction * (baseSpeed + angle / divisionFactor));
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION ATTENTION
+    // LES DEUX MOTEUYRS ONT ETE INVERSES PARCE QUE LE GYROSCOPE NA PAS ETE PLACE DANS LE BON SENS RAAAAAAH
 
-  pami.moteur.moteurGauche(direction * (baseSpeed - angle / divisionFactor));
-  pami.moteur.moteurDroit(direction * (baseSpeed + angle / divisionFactor));
+    
+    pami.moteur.moteurGauche(mg / divisionFactor);
+    pami.moteur.moteurDroit(md / divisionFactor);
+
+    Serial.print("md: ");
+    Serial.print((short)md);
+    Serial.print(" mg: ");
+    Serial.println((short)mg);
+
+    //pami.moteur.moteurGauche(clampValues(mg, maxSpeed * -1, maxSpeed));
+    //pami.moteur.moteurDroit(clampValues(md, maxSpeed * -1, maxSpeed));
+
+    
+  }
+}
+
+
+void conditionnalCorection(){
+
+  char angle = abs(pami.gyro.getAngle(GYROSCOPE_AXIS_Y));
+
+  if (slopeMin < angle && angle < slopeMax && !zbeub){
+    //Serial.print("cc slope: ");
+    Serial.print((short)angle);
+    Serial.println("°.");
+    zbeub = true;
+  }
+
+  if (zbeub && angle < slopeMin) {
+
+    switch (actionSelector) {
+// INUTILE, DÉCOMMENTER AU BESOIN      case 0x01:
+// INUTILE, DÉCOMMENTER AU BESOIN        // Action 0x01 correspond à l'arrêt du robot.
+// INUTILE, DÉCOMMENTER AU BESOIN        pami.moteur.moteurGauche(0);
+// INUTILE, DÉCOMMENTER AU BESOIN        pami.moteur.moteurDroit(0);
+// INUTILE, DÉCOMMENTER AU BESOIN        if (!coOverride){
+// INUTILE, DÉCOMMENTER AU BESOIN          //Serial.println("Overriden speed controls.");
+// INUTILE, DÉCOMMENTER AU BESOIN          coOverride = true;
+// INUTILE, DÉCOMMENTER AU BESOIN        }
+// INUTILE, DÉCOMMENTER AU BESOIN        break;
+// INUTILE, DÉCOMMENTER AU BESOIN
+// INUTILE, DÉCOMMENTER AU BESOIN
+// INUTILE, DÉCOMMENTER AU BESOIN      case 0x02:
+// INUTILE, DÉCOMMENTER AU BESOIN        // Action 0x02 correspond à une rotation de 90°
+// INUTILE, DÉCOMMENTER AU BESOIN
+// INUTILE, DÉCOMMENTER AU BESOIN        targetAngleOffset = 90;
+// INUTILE, DÉCOMMENTER AU BESOIN        //Serial.println("targetAngle = 90");
+// INUTILE, DÉCOMMENTER AU BESOIN        zbeub = false;
+// INUTILE, DÉCOMMENTER AU BESOIN        break;
+// INUTILE, DÉCOMMENTER AU BESOIN
+// INUTILE, DÉCOMMENTER AU BESOIN
+// INUTILE, DÉCOMMENTER AU BESOIN      case 0x03:
+// INUTILE, DÉCOMMENTER AU BESOIN        // Action 0x03 correspond à une rotation de -90°
+// INUTILE, DÉCOMMENTER AU BESOIN
+// INUTILE, DÉCOMMENTER AU BESOIN        targetAngleOffset = -90;
+// INUTILE, DÉCOMMENTER AU BESOIN        //Serial.println("targetAngle = -90.");
+// INUTILE, DÉCOMMENTER AU BESOIN        zbeub = false;
+// INUTILE, DÉCOMMENTER AU BESOIN        break;
+
+
+      case 0x04:
+        // Action 0x02 correspond à une rotation de 90°, puis de l'arrêt sur le bord de la table.
+
+        targetAngleOffset = 90;
+        //Serial.println("target angle = 90.");
+        zbeub = false;
+
+        //Serial.println("conditionnalCorrection has set awaitingDistance to TRUE. Robot will now stop when it reaches an edge of the table.");
+        awaitingDistance = false;
+
+        baseSpeed = 40;
+        divisionFactor = 2;
+
+        timedAction = millis() + 1000;
+        timedActionSelector = 1;
+
+        break;
+
+
+      case 0x05:
+        // Action 0x03 correspond à une rotation de -90°, puis de l'arrêt sur le bord de la table.
+
+        targetAngleOffset = -90;
+        //Serial.println("target angle = -90.");
+        zbeub = false;
+
+        //Serial.println(F"conditionnalCorrection has set awaitingDistance to TRUE. Robot will now stop when it reaches an edge of the table.");
+        awaitingDistance = false;
+
+        baseSpeed = 40;
+        divisionFactor = 2;
+
+        timedAction = millis() + 1000;
+        timedActionSelector = 1;
+
+        break;
+  
+
+      default:
+        //Serial.println(F"Failed to identify the selected action. Please refer to [LINE ---] to select the desired action.");
+        zbeub = false;
+        return;
+    }
+  }
+}
+
+void onEdge() {
+  if (awaitingDistance) {
+    short distance = 0; // OBTENSION DE LA DISTANCE AVEC LE ULTRASON ICI!
+    distance = pami.ultrason.getDistance();
+
+    if (distance == 500) {
+      return;
+    }
+
+    Serial.print("Distance mesurée par le ultrason: ");
+    Serial.println(distance);
+
+
+    // HAUTEUR DE TETECTION CHANGEE POUR EVITER AU MAXIMUM LES FAUX POSITIFS!!
+
+    if ((short)distance >= (short)(70) && (short)distance <= (short)100) { // Hauteur réelle: 55mm (5.5cm). Changer si besoin pour correspondre aux millimètres!!
+      pami.moteur.moteurGauche(0);
+      pami.moteur.moteurDroit(0);
+      coOverride = true;
+      awaitingDistance = false;
+      zbeub = false;
+
+      Serial.println("Robot stopped.");
+
+      dance = true;
+
+      //Serial.println(F"onEdge has overriten the speeds, and taken the lead of speed control.");
+      //Serial.println(F"onEdge has stopped the robot. It will now longer move until it is restarted.");
+    }
+  }
+} 
+
+void nonMandatoryActions(){
+  pami.gpio.set(Head   ,   defaultAngle+SINE);
+  pami.gpio.set(Guitare,   defaultAngle-SINE);
+}
+
+t_radioOrder gestionRadio( void ) {
+  #define RADIO_MSG_SIZE 40
+  char msg[RADIO_MSG_SIZE];
+
+  unsigned char len = pami.radio.getMessage(msg, RADIO_MSG_SIZE);
+
+  if( len == 0 )      return RADIO_NO_ORDER;
+  if( msg[0] != 'T' ) return RADIO_NO_ORDER;
+  if( msg[1] != 'A') return RADIO_NO_ORDER;
+  if( msg[2] == 'G' ) return RADIO_START;
+  if( msg[2] == 'S' ) return RADIO_STOP;
+  return RADIO_NO_ORDER;
 }
